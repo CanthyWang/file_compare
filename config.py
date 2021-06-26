e40783d86abacb255b4d17353306a59609984e9a
CanthyWang <69969448+CanthyWang@users.noreply.github.com>
2021-06-26 23:49:07 +0800

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# The deployment address of the demos module
-demo_host = 'http://filecompare.cathywang.site'
-
-# The deployment address of https://github.com/wang0618/pywebio-chart-gallery repo
-# charts_demo_host = 'http://pywebio-charts.demo.wangweimin.site'
