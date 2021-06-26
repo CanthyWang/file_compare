735e4021cbba0837b356d28ffa1b505c239f2c49
CanthyWang <69969448+CanthyWang@users.noreply.github.com>
2021-06-26 23:45:18 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
 r"""
 .. automodule:: filecompare
-.. automodule:: demos.input_usage
-.. automodule:: demos.output_usage
-.. automodule:: demos.chat_room
-.. automodule:: demos.markdown_previewer
 """
